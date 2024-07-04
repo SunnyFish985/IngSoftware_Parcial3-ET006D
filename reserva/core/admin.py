4d3b296b7ca46679f2fb4bacd82fe5c5495b41f3
joseacuna1221 <134325342+joseacuna1221@users.noreply.github.com>
2024-07-04 00:43:50 -0400

    limpiar
    asd

--- reserva/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Transfer, Chofer
-# Register your models here.
-
-admin.site.register(Categoria)
-admin.site.register(Transfer)
-admin.site.register(Chofer)
